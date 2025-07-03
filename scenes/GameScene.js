fd4e5e8d551d8cab09bfac4d81ee1a1c0e569f0a
magaliek <magrobkal@gmail.com>
2025-07-03 22:59:39 +0200

    first i wanna see if i can do it with collision but im gonna try to switch to the update method

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -10,13 +10,18 @@ export default class GameScene extends Phaser.Scene {
 
     create() {
         this.box = this.add.rectangle(200, 250, 300, 400, 0xb8b3ae);
+        const {x, y, width: boxW, height: boxH} = this.box;
+        const halfW = boxW/2;
+        const halfH = boxH/2;
         this.stroke = 10;
+        this.touchingBorders = new Map();
         this.box.setStrokeStyle(this.stroke, 0x614d3c);
+        this.packed = new Set();
         this.leftWall = this.matter.add.rectangle(
             x - halfW,
             y,
             this.stroke,
-            boxHeight,
+            boxH,
             { isStatic: true, isSensor: true }
         );
 
@@ -24,14 +29,14 @@ export default class GameScene extends Phaser.Scene {
             x + halfW,
             y,
             this.stroke,
-            boxHeight,
+            boxH,
             { isStatic: true, isSensor: true }
         );
 
         this.topWall = this.matter.add.rectangle(
             x,
             y - halfH,
-            boxWidth,
+            boxW,
             this.stroke,
             { isStatic: true, isSensor: true }
         );
@@ -39,14 +44,32 @@ export default class GameScene extends Phaser.Scene {
         this.bottomWall = this.matter.add.rectangle(
             x,
             y + halfH,
-            boxWidth,
+            boxW,
             this.stroke,
             { isStatic: true, isSensor: true }
 
         );
 
         this.matter.world.on('collisionstart', (event) => {
+            event.pairs.forEach(({bodyA, bodyB}) => {
+                const borders = [this.leftWall, this.rightWall, this.topWall, this.bottomWall];
 
+                const isBorderA = borders.includes(bodyA);
+                const isBorderB = borders.includes(bodyB);
+
+                const objA = bodyA.gameObject;
+                const objB = bodyB.gameObject;
+
+                if (isBorderA && objB?.setTint) {
+                    const count = this.touchingBorders.get(objB) || 0;
+                    this.touchingBorders.set(objB, count+1);
+                    objB.setTint(0xFF0000);
+                } else if (isBorderB && objA?.setTint) {
+                    const count = this.touchingBorders.get(objA) || 0;
+                    this.touchingBorders.set(objA, count+1);
+                    objA.setTint(0xFF0000);
+                }
+            });
         });
 
         this.matter.world.on('collisionend', (event) => {
@@ -60,9 +83,27 @@ export default class GameScene extends Phaser.Scene {
                 const objB = bodyB.gameObject;
 
                 if (isBorderA && objB?.clearTint) {
-                    objB.clearTint();
+                    let count = this.touchingBorders.get(objB) -1;
+                    this.touchingBorders.set(objB, count);
+                    if (count === undefined) {
+                        console.warn("Unexpected: collisionend fired without collisionstart.");
+                        count = 1;
+                    }
+                    if (count <= 0) {
+                        this.touchingBorders.delete(objB);
+                        objB.clearTint();
+                    }
                 } else if (isBorderB && objA.clearTint) {
-                    objA.clearTint();
+                    let count = this.touchingBorders.get(objA) - 1;
+                    this.touchingBorders.set(objA, count);
+                    if (count === undefined) {
+                        console.warn("Unexpected: collisionend fired without collisionstart.");
+                        count = 1;
+                    }
+                    if (count <= 0) {
+                        this.touchingBorders.delete(objA);
+                        objA.clearTint();
+                    }
                 }
             });
         });
@@ -71,15 +112,16 @@ export default class GameScene extends Phaser.Scene {
         const hairdryerShapes = this.cache.json.get('hairdryerShape');
         this.hairdryer = this.matter.add.sprite(700, 200, 'hairdryer', null, {
             shape: hairdryerShapes['hairdryer'],
-            isStatic: true,
+            isStatic: false,
             restitution: 0,
-            friction: 1,
-            frictionStatic: 1,
-            frictionAir: 0.05
+            friction: 0,
+            frictionStatic: 0,
+            frictionAir: 0,
+            inertia: Infinity
         });
         this.hairdryer.setScale(0.6);
         this.hairdryer.setInteractive();
-
+        this.hairdryer.setIgnoreGravity(true);
         this.input.setDraggable(this.hairdryer);
 
         this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
@@ -90,5 +132,21 @@ export default class GameScene extends Phaser.Scene {
             gameObject.y = dragY;
         });
 
+    }
+
+    update() {
+        const isInBox = (sprite) => {
+            const bounds = sprite.body.bounds;
+            const boxBounds = this.box.getBounds();
+            const width = bounds.max.x - bounds.min.x;
+            const height = bounds.max.y - bounds.min.y;
+
+            const isInBox = bounds.max.x < boxBounds.right &&
+                            bounds.min.x > boxBounds.left &&
+                            bounds.max.y > boxBounds.top &&
+                            bounds.min.y < boxBounds.bottom;
+
+
+        }
     }
 }
