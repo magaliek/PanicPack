b75b32866d3691bbf3d2cad537aaf7cf6efff8f8
magaliek <magrobkal@gmail.com>
2025-07-12 18:32:49 -0400

    matterjs is very complicated and doesnt work when you need to flip or rotate (maybe im just a noob but i tried for hours) so i decided to not make my sprites have bodies. ill try to manually detect collisions using simple math.

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -5,9 +5,7 @@ export default class GameScene extends Phaser.Scene {
 
     preload() {
         this.load.image('hairdryer', 'assets/hairdryer.png');
-        this.load.json('hairdryerShape', 'assets/hairdryer.json');
         this.load.image('laptop', 'assets/laptop.png');
-        this.load.json('laptopShape', 'assets/laptop.json');
         this.load.image('flipH', 'assets/flip.png');
         this.load.image('flipV', 'assets/rotate.png');
     }
@@ -60,96 +58,19 @@ export default class GameScene extends Phaser.Scene {
         this.flipH.setInteractive();
         this.flipV.setInteractive();
 
-        const hairdryerShapes = this.cache.json.get('hairdryerShape');
-        this.hairdryer = this.matter.add.sprite(700, 200, 'hairdryer', null, {
-            shape: hairdryerShapes['hairdryer'],
-            isStatic: false,
-            restitution: 0,
-            friction: 0,
-            frictionStatic: 0,
-            frictionAir: 0,
-            inertia: Infinity
-        });
+        this.hairdryer = this.add.sprite(700, 200, 'hairdryer');
         this.hairdryer.setScale(0.6);
         this.hairdryer.setInteractive();
-        this.hairdryer.setIgnoreGravity(true);
         this.input.setDraggable(this.hairdryer);
         this.sprites.push(this.hairdryer);
 
-        const laptopShapes = this.cache.json.get('laptopShape');
-        this.laptop = this.matter.add.sprite(800, 200, 'laptop', null, {
-            shape: laptopShapes['laptop'],
-            isStatic: false,
-            restitution: 0,
-            friction: 0,
-            frictionStatic: 0,
-            frictionAir: 0,
-            inertia: Infinity
-        });
+        this.laptop = this.add.sprite(800, 200, 'laptop');
         this.laptop.setScale(0.5);
         this.laptop.setInteractive();
-        this.laptop.setIgnoreGravity(true);
         this.input.setDraggable(this.laptop);
         this.sprites.push(this.laptop);
 
         this.selected = this.laptop;
-        this.matter.world.on('collisionstart', (event) => {
-            event.pairs.forEach(({bodyA, bodyB}) => {
-                const borders = [this.leftWall, this.rightWall, this.topWall, this.bottomWall];
-
-                const isBorderA = borders.includes(bodyA);
-                const isBorderB = borders.includes(bodyB);
-
-                const objA = bodyA.gameObject;
-                const objB = bodyB.gameObject;
-
-                if (isBorderA && objB?.setTint) {
-                    const count = this.touchingBorders.get(objB) || 0;
-                    this.touchingBorders.set(objB, count+1);
-                    objB.setTint(0xFF0000);
-                } else if (isBorderB && objA?.setTint) {
-                    const count = this.touchingBorders.get(objA) || 0;
-                    this.touchingBorders.set(objA, count+1);
-                    objA.setTint(0xFF0000);
-                }
-            });
-        });
-
-        this.matter.world.on('collisionend', (event) => {
-            event.pairs.forEach(({ bodyA, bodyB }) => {
-                const borders = [this.leftWall, this.rightWall, this.topWall, this.bottomWall];
-
-                const isBorderA = borders.includes(bodyA);
-                const isBorderB = borders.includes(bodyB);
-
-                const objA = bodyA.gameObject;
-                const objB = bodyB.gameObject;
-
-                if (isBorderA && objB?.clearTint) {
-                    let count = this.touchingBorders.get(objB) -1;
-                    this.touchingBorders.set(objB, count);
-                    if (count === undefined) {
-                        console.warn("Unexpected: collisionend fired without collisionstart.");
-                        count = 1;
-                    }
-                    if (count <= 0) {
-                        this.touchingBorders.delete(objB);
-                        objB.clearTint();
-                    }
-                } else if (isBorderB && objA.clearTint) {
-                    let count = this.touchingBorders.get(objA) - 1;
-                    this.touchingBorders.set(objA, count);
-                    if (count === undefined) {
-                        console.warn("Unexpected: collisionend fired without collisionstart.");
-                        count = 1;
-                    }
-                    if (count <= 0) {
-                        this.touchingBorders.delete(objA);
-                        objA.clearTint();
-                    }
-                }
-            });
-        });
 
         this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
             dragY= Phaser.Math.Clamp(dragY, 24, 600-24);
@@ -181,13 +102,13 @@ export default class GameScene extends Phaser.Scene {
     /*scoring system here later*/
     update() {
         const isInBox = (sprite) => {
-            const bounds = sprite.body.bounds;
+            const bounds = sprite.getBounds(); // no body required
             const boxBounds = this.box.getBounds();
 
-            const inside = bounds.max.x < boxBounds.right &&
-                bounds.min.x > boxBounds.left &&
-                bounds.max.y > boxBounds.top &&
-                bounds.min.y < boxBounds.bottom;
+            const inside = bounds.right < boxBounds.right &&
+                bounds.left > boxBounds.left &&
+                bounds.bottom < boxBounds.bottom &&
+                bounds.top > boxBounds.top;
 
             if (inside && (sprite.tintTopLeft !== 0xFF0000)) {
                 this.packed.add(sprite);
@@ -195,7 +116,7 @@ export default class GameScene extends Phaser.Scene {
                 this.packed.delete(sprite);
             }
             return inside;
-        }
+        };
         this.sprites.forEach(sprite => isInBox(sprite));
     }
 }
